f24bb89ee649597e05436603d56a6f161a7f2fc0
sawanoboly <sawanoboriyu@higanworks.com>
2012-10-02 17:54:06 +0900

    Initial

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "HiganWorks LLC"
+maintainer_email "sawanoboriyu@higanworks.com"
+license          "MIT"
+description      "Installs/Configures riak-cluster"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
---
+++ recipes/default.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: riak-cluster
+# Recipe:: default
+#
+# Copyright 2012, HiganWorks LLC
+#
+# license: MIT
+
+store_dir = "#{Chef::Config[:file_cache_path]}/riak"
+
+# todo Attributes
+# http://downloads.basho.com.s3-website-us-east-1.amazonaws.com/riak/1.2/1.2.0/ubuntu/precise/riak_1.2.0-1_amd64.deb
+riak_ = "/1.2/1.2.0/ubuntu/precise/riak_1.2.0-1_amd64.deb"
+
+
+directory store_dir do
+  mode 00700
+end
+
+remote_file "#{store_dir}/riak_1.2.0-1_amd64.deb" do
+  source "http://downloads.basho.com.s3-website-us-east-1.amazonaws.com/riak/1.2/1.2.0/ubuntu/precise/riak_1.2.0-1_amd64.deb"
+  action :create_if_missing
+end
+
+package "riak" do
+  Chef::Provider::Package::Dpkg
+  source "#{store_dir}/riak_1.2.0-1_amd64.deb"
+  action :install
+end
